ecd5888f54a58f680592609e54430a0930a17ef1
unknown <soulmamoudou0@gmail.com>
2025-11-10 18:12:22 +0000

    Configuration pour CI/CD

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5175',
+    origin: process.env.CORS_ORIGIN || '',
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
   await app.listen(process.env.PORT || 8003);
